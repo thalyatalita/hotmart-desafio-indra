ab3a3ab115975680c78d58eae171459d42dadccc
thalyatalita <thalya.amorim17@gmail.com>
2020-06-07 14:42:53 -0300

    versao 2

--- src/test/java/desafio/qa/page/HotmartPage.java
+++ src/test/java/desafio/qa/page/HotmartPage.java
@@ -21,96 +21,147 @@ public class HotmartPage extends CorePage<HotmartPage> {
 	}
 
 	// MAPEAMENTO
-	
+
 	@FindBy(xpath = "//div[@class='header__navigation-plattaform']//span[contains(text(),'Cadastre-se')]")
 	private WebElement cadastrar;
-	
+
 	@FindBy(xpath = "//*[contains(@id,'signup')]/div[1]/input[1]")
 	private WebElement nome;
-	
+
 	@FindBy(xpath = "//*[contains(@id,'signup')]/div[2]/input[1]")
 	private WebElement email;
-	
+
 	@FindBy(xpath = "//*[contains(@id,'signup')]/div[3]/input[1]")
 	private WebElement senha;
-	
+
 	@FindBy(xpath = "//label[@class='hot-custom-control__label']")
 	private WebElement termoUso;
-	
+
 	@FindBy(xpath = "//span[contains(text(),'Continuar')]")
 	private WebElement btnContinuar;
-	
+
 	@FindBy(xpath = "//p[contains(text(),'Já existe um usuário cadastrado com este e-mail')]")
-	private WebElement alert;
-	
+	private WebElement alertUsuarioCadastrado;
+
+	@FindBy(xpath = "//p[contains(text(),'Aceite os Termos de Uso da Hotmart para continuar.')]")
+	private WebElement alertTermoUso;
+
 	@FindBy(xpath = "//div[text()='Nome Completo é obrigatório']")
-	private WebElement mgsNome;
-	
+	private WebElement msgCampoObrigatorioNome;
+
 	@FindBy(xpath = "//div[text()='Email é obrigatório']")
-	private WebElement mgsEmail;
-	
+	private WebElement msgCampoObrigatorioEmail;
+
 	@FindBy(xpath = "//div[text()='Senha é obrigatório']")
-	private WebElement mgsSenha;
-	
+	private WebElement msgCampoObrigatorioSenha;
+
+	@FindBy(xpath = "//div[text()='Digite um endereço de Email válido']")
+	private WebElement msgEmailInvalido;
+
+	@FindBy(xpath = "//div[text()='No mínimo 7 caracteres, com ao menos uma letra e um número.']")
+	private WebElement msgSenhaIncorreta;
+
 	//METODOS DE VALIDAÇÂO DE TEXTO
-	
-	public void validarAlerta(String texto) {
+
+	public void validarMensagemCampoObrigatorioNome(String texto) {
 		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
-		driverWait.until(ExpectedConditions.visibilityOf(alert));
-		Assert.assertEquals(texto, alert.getText());
-	}
-	
-	public void validarMensagemErroCampoNome(String texto) {
-		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
-		driverWait.until(ExpectedConditions.visibilityOf(mgsNome));
-		Assert.assertEquals(texto, mgsNome.getText());
-	}
-	
-	public void validarMensagemErroCampoEmail(String texto) {
-		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
-		driverWait.until(ExpectedConditions.visibilityOf(mgsEmail));
-		Assert.assertEquals(texto, mgsEmail.getText());
-	}
-	
-	public void validarMensagemErroCampoSenha(String texto) {
-		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
-		driverWait.until(ExpectedConditions.visibilityOf(mgsSenha));
-		Assert.assertEquals(texto, mgsSenha.getText());
+		driverWait.until(ExpectedConditions.visibilityOf(msgCampoObrigatorioNome));
+		Assert.assertEquals(texto, msgCampoObrigatorioNome.getText());
 	}
 
-	// METODOS DE CLICK
-	
+	public void validarMensagemCampoObrigatorioEmail(String texto) {
+		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
+		driverWait.until(ExpectedConditions.visibilityOf(msgCampoObrigatorioEmail));
+		Assert.assertEquals(texto, msgCampoObrigatorioEmail.getText());
+	}
+
+	public void validarMensagemCampoObrigatorioSenha(String texto) {
+		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
+		driverWait.until(ExpectedConditions.visibilityOf(msgCampoObrigatorioSenha));
+		Assert.assertEquals(texto, msgCampoObrigatorioSenha.getText());
+	}
+
+	public void validarMensagemEmailInvalido(String texto) {
+		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
+		driverWait.until(ExpectedConditions.visibilityOf(msgEmailInvalido));
+		Assert.assertEquals(texto, msgEmailInvalido.getText());
+	}
+
+	public void validarMensagemSenhaInvalido(String texto) {
+		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
+		driverWait.until(ExpectedConditions.visibilityOf(msgSenhaIncorreta));
+		Assert.assertEquals(texto, msgSenhaIncorreta.getText());
+	}
+
+	//METODOS DE CLICK
+
 	public void clicarCadastrar() {
 		aguardarElementoVisivel(cadastrar);
 		click(cadastrar);
 	}
-	
+
 	public void clicarTermoUso() {
 		aguardarElementoVisivel(termoUso);
 		click(termoUso);
 	}
-	
+
 	public void clicarBtnContinuar() {
 		aguardarElementoVisivel(btnContinuar);
 		moverCursorPara(btnContinuar);
 		click(btnContinuar);
 	}
-	
-	// MÈTODOS DE PREENCHIMENTOS DE CAMPOS
+
+	//METODOS DE PREENCHIMENTOS DE CAMPOS
 
 	public void preencherNomeCompleto() {
 		aguardarElementoVisivel(nome);
-		preencherCampo(nome, "Thalya Amorim");
+		preencherCampo(nome, "Usuário Teste");
 	}
-	
+
 	public void preencherEmail() {
 		aguardarElementoVisivel(email);
-		preencherCampo(email, "desafio@desafio.com");
+		preencherCampo(email, "desafio@teste.com");
 	}
-	
+
 	public void preencherSenha() {
 		aguardarElementoVisivel(senha);
-		preencherCampo(senha, "123456a");
+		preencherCampo(senha, "2f36jw9");
+	}
+
+	//METODOS DE PREENCHIMENTOS INVALIDOS DE CAMPOS
+
+	public void preencherEmailInvalido() {
+		aguardarElementoVisivel(email);
+		preencherCampo(email, "desafio@");
+	}
+
+	public void preencherSenhaInvalida() {
+		aguardarElementoVisivel(senha);
+		preencherCampo(senha, "2d6j3g");
+	}
+
+	public void preencherSenhaInvalidaApenasNumero() {
+		aguardarElementoVisivel(senha);
+		preencherCampo(senha, "1368249");
+	}
+
+	public void preencherSenhaInvalidaApenasLetra() {
+		aguardarElementoVisivel(senha);
+		preencherCampo(senha, "abyelks");
+	}
+
+	//METODOS DE VALIDAÇÂO DE ALERT
+
+	public void validarAlerta(String texto) {
+		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
+		driverWait.until(ExpectedConditions.visibilityOf(alertUsuarioCadastrado));
+		Assert.assertEquals(texto, alertUsuarioCadastrado.getText());
+	}
+
+	public void validarAlertaTermoUso(String texto) {
+		WebDriverWait driverWait = new WebDriverWait(CoreDriver.getDriver(), 30);
+		driverWait.until(ExpectedConditions.visibilityOf(alertTermoUso));
+		Assert.assertEquals(texto, alertTermoUso.getText());
 	}
 
 }
--- src/test/java/desafio/qa/test/HotmartTest.java
+++ src/test/java/desafio/qa/test/HotmartTest.java
@@ -1,13 +1,76 @@
 package desafio.qa.test;
 
 import org.junit.Test;
-
 import desafio.qa.core.CoreBaseTest;
 import desafio.qa.page.HotmartPage;
 import desafio.qa.util.Property;
 import desafio.qa.util.Utils;
 
 public class HotmartTest extends CoreBaseTest {
+	
+	@Test
+	public void validarSenhaInvalidaApenasNumeroTest() {
+		
+		HotmartPage hotmartPage = new HotmartPage();
+		
+		driver.navigate().to(Property.URL);
+		
+		hotmartPage.preencherNomeCompleto();
+		hotmartPage.preencherEmail();
+		hotmartPage.preencherSenhaInvalidaApenasNumero();
+		hotmartPage.clicarTermoUso();
+		hotmartPage.clicarBtnContinuar();
+		hotmartPage.validarMensagemSenhaInvalido("No mínimo 7 caracteres, com ao menos uma letra e um número.");
+		Utils.takeScreenshot(driver, "CadastroInvalidoUsuario", "SenhaInvalidaApenasNumero");
+	}
+	
+	@Test
+	public void validarSenhaInvalidaApenasLetrasTest() {
+		
+		HotmartPage hotmartPage = new HotmartPage();
+		
+		driver.navigate().to(Property.URL);
+		
+		hotmartPage.preencherNomeCompleto();
+		hotmartPage.preencherEmail();
+		hotmartPage.preencherSenhaInvalidaApenasLetra();
+		hotmartPage.clicarTermoUso();
+		hotmartPage.clicarBtnContinuar();
+		hotmartPage.validarMensagemSenhaInvalido("No mínimo 7 caracteres, com ao menos uma letra e um número.");
+		Utils.takeScreenshot(driver, "CadastroInvalidoUsuario", "SenhaInvalidaApenasLetras");
+	}
+	
+	@Test
+	public void validarSenhaInvalidaTest() {
+		
+		HotmartPage hotmartPage = new HotmartPage();
+		
+		driver.navigate().to(Property.URL);
+		
+		hotmartPage.preencherNomeCompleto();
+		hotmartPage.preencherEmail();
+		hotmartPage.preencherSenhaInvalida();
+		hotmartPage.clicarTermoUso();
+		hotmartPage.clicarBtnContinuar();
+		hotmartPage.validarMensagemSenhaInvalido("No mínimo 7 caracteres, com ao menos uma letra e um número.");
+		Utils.takeScreenshot(driver, "CadastroInvalidoUsuario", "SenhaInvalida");
+	}
+	
+	@Test
+	public void validarEmailInvalidoTest() {
+		
+		HotmartPage hotmartPage = new HotmartPage();
+		
+		driver.navigate().to(Property.URL);
+		
+		hotmartPage.preencherNomeCompleto();
+		hotmartPage.preencherEmailInvalido();
+		hotmartPage.preencherSenha();
+		hotmartPage.clicarTermoUso();
+		hotmartPage.clicarBtnContinuar();
+		hotmartPage.validarMensagemEmailInvalido("Digite um endereço de Email válido");
+		Utils.takeScreenshot(driver, "CadastroInvalidoUsuario", "cadastroComEmailInvalido");
+	}
 	
 	@Test
 	public void validarCamposObrigatorioTest() {
@@ -18,9 +81,9 @@ public class HotmartTest extends CoreBaseTest {
 		
 		hotmartPage.clicarBtnContinuar();
 		
-		hotmartPage.validarMensagemErroCampoNome("Nome Completo é obrigatório");
-		hotmartPage.validarMensagemErroCampoEmail("Email é obrigatório");
-		hotmartPage.validarMensagemErroCampoSenha("Senha é obrigatório");
+		hotmartPage.validarMensagemCampoObrigatorioNome("Nome Completo é obrigatório");
+		hotmartPage.validarMensagemCampoObrigatorioEmail("Email é obrigatório");
+		hotmartPage.validarMensagemCampoObrigatorioSenha("Senha é obrigatório");
 		
 		Utils.takeScreenshot(driver, "CadastroUsuario", "validarCamposObrigatorios");
 	}
@@ -55,5 +118,21 @@ public class HotmartTest extends CoreBaseTest {
 		hotmartPage.validarAlerta("Já existe um usuário cadastrado com este e-mail");
 		Utils.takeScreenshot(driver, "CadastroUsuario", "cadastroExistente");
 	}
+	
+	@Test
+	public void validarTermoUsoTest() {
+		
+		HotmartPage hotmartPage = new HotmartPage();
+		
+		driver.navigate().to(Property.URL);
+		
+		hotmartPage.preencherNomeCompleto();
+		hotmartPage.preencherEmail();
+		hotmartPage.preencherSenha();
+		hotmartPage.clicarBtnContinuar();
+		hotmartPage.validarAlertaTermoUso("Aceite os Termos de Uso da Hotmart para continuar.");
+		Utils.takeScreenshot(driver, "CadastroUsuario", "termoUso");
+	}
+	
 
 }
